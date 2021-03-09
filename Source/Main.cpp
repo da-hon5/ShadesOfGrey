e2a0c1a09701419debfc29ed5599f9faa69057d8
Hannes Bradl <hannes.bradl@student.kug.ac.at>
2021-03-09 17:00:15 +0000

    Update Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,12 @@
 /*
   ==============================================================================
 
-    This file contains the startup code for a PIP.
+    Author:  Hannes Bradl, hbradl@gmx.at
+
+    This is free source code: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
   ==============================================================================
 */
