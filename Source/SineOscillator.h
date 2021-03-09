47a57beb40563c076d3ba57ad96c907fd74a84c8
Hannes Bradl <hannes.bradl@student.kug.ac.at>
2021-03-09 17:01:53 +0000

    Update SineOscillator.h

--- Source/SineOscillator.h
+++ Source/SineOscillator.h
@@ -1,9 +1,12 @@
 /*
   ==============================================================================
 
-    SineOscillator.h
-    Created: 5 Nov 2020 10:51:28am
-    Author:  hon5
+    Author:  Hannes Bradl, hbradl@gmx.at
+
+    This is free source code: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
   ==============================================================================
 */
